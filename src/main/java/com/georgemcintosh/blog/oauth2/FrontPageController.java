c1ba36a577a3d383f271bbda70c147110946f91d
George McIntosh <george@georgemcintosh.com>
2023-05-17 10:25:04 +0100

    wip

--- src/main/java/com/georgemcintosh/blog/oauth2/FrontPageController.java
+++ src/main/java/com/georgemcintosh/blog/oauth2/FrontPageController.java
@@ -1,6 +1,10 @@
 package com.georgemcintosh.blog.oauth2;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +14,9 @@ import java.util.Map;
 
 @Controller
 public class FrontPageController {
+
+    @Autowired
+    private OAuth2AuthorizedClientService clientService;
 
     @RequestMapping("/")
     public ModelAndView index(@AuthenticationPrincipal OAuth2User principal) {
